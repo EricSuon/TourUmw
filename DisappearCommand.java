1af00a9257f787ee625aeff758faa9a6cfad40a2
krackemadison <mkracke@mail.umw.edu>
2025-12-01 20:49:26 -0500

    MK

--- DisappearCommand.java
+++ DisappearCommand.java
@@ -8,7 +8,10 @@ public class DisappearCommand implements UserInputCommand {
     private String goneItem;
     private boolean isValidCommand = true;
 
-
+    /**
+     * Disappear Class constructor
+     * @param input the String to show the remove action
+     */
     public DisappearCommand(String input) {
         String[] parts = input.split("\\s+", 2);
 
@@ -20,6 +23,10 @@ public class DisappearCommand implements UserInputCommand {
         }
     }
 
+    /**
+     * Results from removing
+     * @return the String showing the remove results
+     */
     public String carryOut() {
         if(!isValidCommand) {
             return "What do you want to disappear? Specify the item you want.";
@@ -27,14 +34,17 @@ public class DisappearCommand implements UserInputCommand {
 
         TourStatus tour = TourStatus.getInstance();
 
+        //get item from backpack
         Item disappear = tour.getItemFromBackpack(goneItem);
 
-
+        //check for item
         if (disappear == null) {
             return goneItem + " is not here, you cannot make it disappear.";
         }
 
         String goneMessage = disappear.getActionTwo();
+
+        //permanently remove item from backpack
         if (goneMessage == null) {
             tour.removeFromBackpack(disappear);
             return goneItem + "disappears without a trace!";
