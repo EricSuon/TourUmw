c8a1b9d046a048ac61699c381fcb1755ff3d9464
krackemadison <mkracke@mail.umw.edu>
2025-12-01 22:25:47 -0500

    MK Door

--- Door.java
+++ Door.java
@@ -8,6 +8,9 @@ public class Door {
     private final Location from;
     /** Destination location. */
     private final Location to;
+    /** Locking Doors */
+    private boolean isLocked;
+    private String keyItemName;
 
     /**
      * Constructs a Door.
@@ -21,6 +24,13 @@ public class Door {
         this.to = to;
     }
 
+    public Door(char direction, Location from, Location to, String keyItemName) {
+        this.direction = Character.toLowerCase(direction);
+        this.from = from;
+        this.to = to;
+        this.isLocked = true;
+        this.keyItemName = keyItemName;
+    }
     /**
      * Gets the door direction.
      * @return direction char
@@ -38,4 +48,32 @@ public class Door {
      * @return to location
      */
     public Location getTo() { return to; }
+
+    /**
+     * Gets status of locked door
+     * @return true if door is currently locked
+     */
+    public boolean getIsLocked() { return isLocked; }
+    /**
+     * Sets locked door statys
+     * @param locked true to lock door, false to unlock.
+     */
+    public void setIsLocked(boolean locked) { this.isLocked = locked; }
+    /**
+     * gets name of the key item required to unlock this door
+     * @return key name or null
+     */
+    public String getKeyItemName () { return keyItemName; }
+
+    /**
+     * Check if item is correct key to unlock
+     * @param itemName
+     * @return true if door is unlocked and correct key used
+     */
+    public boolean canUnlock(String itemName) {
+        if (!isLocked || keyItemName == null || itemName == null) {
+            return false;
+        }
+        return keyItemName.equalsIgnoreCase(itemName);
+    }
 }
